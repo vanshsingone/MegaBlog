c28d6d986915557942e4f58793e698478ddfa7b3
vanshsingone <vanshsingone@gmail.com>
2026-01-27 20:14:42 +0530

    feat: add Header, Footer, and Button components; implement Container and Input components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import "./App.css";
 import authService from "./appwrite/auth";
 import {login,logout} from "./store/authSlice"
-import { Footer } from "./components";
+import {Header, Footer } from "./components";
 
 
 function App() {
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,13 @@
-import Header from "./header/header"
-import Footer from "./footer/footer"
+import Select from "./Select";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 export {
-    Header,
-    Footer
+     Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
 }
